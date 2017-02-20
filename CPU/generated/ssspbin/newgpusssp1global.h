618d19d4e20a692acf521a63ff8dce6bcf1611bb
UNNIKRISHNAN CHERAMANGALATH <cnunnikrishnan@gmail.com>
2017-02-20 14:34:38 +0530

    commit on Feb 20

--- CPU/generated/ssspbin/newgpusssp1global.h
+++
@@ -1,1 +0,0 @@
-int FALC_THREADS;
--- GPU/generated/mstbin/singlemst_orderedglobal.h
+++
@@ -1,9 +0,0 @@
-int FALC_THREADS;
- struct node {
-/*-1*/int   lock ;
-int  set ;
-int  src ;
-int  dst ;
-/*-1*/int   weight ;
-};
-__device__ int falcgraphpoint,falcgraphedge;
